fbf5eb408a48e789a8315f3fb73959120ddbd8f2
Crispin Tuballa <cristuballa@gmail.com>
2024-02-15 22:18:17 +1300

    initial commit

---
+++ Application/Data/IApplicationDbContext.cs
@@ -0,0 +1,22 @@
+﻿using Domain.Customers;
+using Domain.Orders;
+using Domain.Products;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Infrastructure;
+
+namespace Application.Data;
+
+public interface IApplicationDbContext
+{
+    DbSet<Customer> Customers { get; set; }
+
+    DbSet<Order> Orders { get; set; }
+
+    DbSet<OrderSummary> OrderSummaries { get; set; }
+
+    DbSet<Product> Products { get; set; }
+
+    DatabaseFacade Database { get; }
+
+    Task<int> SaveChangesAsync(CancellationToken cancellationToken = default);
+}
